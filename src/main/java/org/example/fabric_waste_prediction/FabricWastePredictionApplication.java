e6a9a3e5a5acf02cd5ae240de01ced78cc7e5b15
IT24100442 <it24100442@my.sliit.lk>
2026-02-10 06:15:52 +0530

    Initial commit

---
+++ src/main/java/org/example/fabric_waste_prediction/FabricWastePredictionApplication.java
@@ -0,0 +1,13 @@
+package org.example.fabric_waste_prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricWastePredictionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricWastePredictionApplication.class, args);
+    }
+
+}
