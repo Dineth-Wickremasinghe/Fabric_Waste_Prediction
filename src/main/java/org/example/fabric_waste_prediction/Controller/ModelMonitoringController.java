6ee4b0426c6bdf65028c2cea5cf0e97a77fabae3
Dineth Wickremasinghe <dinethwickremasinghe3@gmail.com>
2026-02-21 07:25:32 +0530

    model feedback UI init

---
+++ src/main/java/org/example/fabric_waste_prediction/Controller/ModelMonitoringController.java
@@ -0,0 +1,18 @@
+package org.example.fabric_waste_prediction.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/monitoring")
+public class ModelMonitoringController {
+
+    @GetMapping(path = "/show")
+    public String showDashboard(){
+        return "model_feedback";
+    }
+
+
+}
